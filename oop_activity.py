ae1cf62f692f92857b103a6749c5b9d7d47bae5d
Joyce Hong <jkim2027@gmail.com>
2023-03-03 22:18:20 -0500

    Update oop_activity.py

--- oop_activity.py
+++ oop_activity.py
@@ -14,14 +14,15 @@ class Zoo:
 
 
     def remove_animal(self, name):
-        for animal in self.animals_in_zoo():
-            if name in animal:
-                return name
+        for animal in self.animals_in_zoo:
+            if animal.name == name:
+                self.animals_in_zoo.remove(animal)
+                print(f"{animal.species} has been removed.")
     
     def get_animals_by_species(self, species):
         list_by_species = []
         for animal in self.animals_in_zoo:
-            if species in animal:
+            if animal.species == species:
                 list_by_species.append(animal.name)
         return list_by_species
     
@@ -33,15 +34,16 @@ class Zoo:
         return avg
 
 
-
-
 lion = ZooAnimal("Simba", "Lion", 5)
+lion2 = ZooAnimal("Nala", "Lion", 5)
 giraffe = ZooAnimal("Melman", "Giraffe", 7)
 elephant = ZooAnimal("Dumbo", "Elephant", 10)
 zoo = Zoo()
 zoo.add_animal(lion)
+zoo.add_animal(lion2)
 zoo.add_animal(giraffe)
 zoo.add_animal(elephant)
 print(zoo.average_age())
-print(zoo.remove_animal("Simba"))
-# print(zoo.get_animals_by_species("Giraffe"))
+print(zoo.remove_animal("Melman"))
+giraffes = zoo.get_animals_by_species("Lion")
+print(giraffes) # Output: ["Melman"]
